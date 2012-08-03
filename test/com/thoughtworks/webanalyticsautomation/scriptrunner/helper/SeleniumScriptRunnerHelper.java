ccc5a5202d64dcde71a277d137d6c9358341dbbd
Anand Bagmar <abagmar@gmail.com>
2012-08-03 15:19:01 +0530

    Update WAAT version and update the seleinum-webdriver version

--- test/com/thoughtworks/webanalyticsautomation/scriptrunner/helper/SeleniumScriptRunnerHelper.java
+++ test/com/thoughtworks/webanalyticsautomation/scriptrunner/helper/SeleniumScriptRunnerHelper.java
@@ -32,7 +32,7 @@ public class SeleniumScriptRunnerHelper extends ScriptRunnerHelper {
     public void startDriver()
     {
         logger.info ("Starting Selenium");
-        String command = "java -jar " + Utils.currentDirectory() + "\\lib\\test\\webTestingFrameworks\\webdriver\\selenium-server-standalone-2.0b1.jar";
+        String command = "java -jar " + Utils.currentDirectory() + "\\lib\\test\\webTestingFrameworks\\webdriver\\selenium-server-standalone-2.25.0.jar";
         logger.info (command);
         this.uiDriverThreadRunner = new UIDriverThreadRunner(logger);
         this.uiDriverThreadRunner.runInThread(command);
